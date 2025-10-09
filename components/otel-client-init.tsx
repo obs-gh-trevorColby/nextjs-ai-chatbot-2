df9b10ee9a5165841fc217bb193067b0243e7ae6
trevor.colby <trevor.colby@observeinc.com>
2025-10-09 14:54:06 -0700

    Add console logging for instrumentation test

--- components/otel-client-init.tsx
+++ components/otel-client-init.tsx
@@ -3,8 +3,11 @@
 import { useEffect } from "react";
 
 export function OtelClientInit() {
+
   useEffect(() => {
+    console.log('OtelClientInit!!!!!!!');
     if (typeof window !== "undefined") {
+      console.log('OtelClientInit: window is defined!!!!!!!!');
       // Import and initialize client-side OpenTelemetry
       import("../otel-client").then(({ initOtel }) => {
         initOtel();
