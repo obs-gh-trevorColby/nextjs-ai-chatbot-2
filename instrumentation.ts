b7699b0adca074897635ff0d0bc5163b51007956
trevor.colby <trevor.colby@observeinc.com>
2025-10-15 17:26:31 -0700

    Manual test of instrumentation for vercel template

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,5 +1,12 @@
 import { registerOTel } from "@vercel/otel";
 
-export function register() {
-  registerOTel({ serviceName: "ai-chatbot" });
+// export function register() {
+//   registerOTel({ serviceName: "ai-chatbot" });
+// }
+
+export async function register() {
+  // registerOTel({ serviceName: "ai-chatbot" });
+  if (process.env.NEXT_RUNTIME === 'nodejs') {
+    await import('./instrumentation.node')
+  }
 }
